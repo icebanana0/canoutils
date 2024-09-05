70d30622f8ecf5e0b53efad326dd9dd5ccda9dfa
icebanana0 <isaac.song12345.r.r@gmail.com>
2024-09-04 17:51:09 -0700

    Added getopt for option handling

--- src/hostname/hostname.c
+++ src/hostname/hostname.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <getopt.h>
 
 #define NAME "hostname (canoutils)"
 #define VERSION "1.0.0"
@@ -35,27 +36,28 @@ static void printShortHostname(void) {
     if (gethostname(hostname, sizeof(hostname)) == 0) {
         printf("Hostname: %s\n", hostname);
     } else {
-        perror("gethostname");
+        fprintf(stderr, "Error getting hostname: %s\n", strerror(errno));
     }
 }
 
 static void printDNSDomainName(void) {
     char hostname[256];
-    char *domain;
-    if (gethostname(hostname, sizeof(hostname)) == 0) {
-        struct hostent *host = gethostbyname(hostname);
-        if (host && host->h_name) {
-            domain = strchr(host->h_name, '.');
-            if (domain) {
-                printf("DNS Domain Name: %s\n", domain + 1);
-            } else {
-                printf("DNS Domain Name: (none)\n");
-            }
-        } else {
-            perror("gethostbyname");
-        }
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        fprintf(stderr, "Error getting hostname: %s\n", strerror(errno));
+        return;
+    }
+
+    struct hostent *host = gethostbyname(hostname);
+    if (!host) {
+        fprintf(stderr, "Error getting DNS domain name: %s\n", hstrerror(h_errno));
+        return;
+    }
+
+    char *domain = strchr(host->h_name, '.');
+    if (domain) {
+        printf("DNS Domain Name: %s\n", domain + 1);
     } else {
-        perror("gethostname");
+        printf("DNS Domain Name: (none)\n");
     }
 }
 
@@ -64,21 +66,21 @@ static void printIPAddresses(void) {
     struct addrinfo hints, *res, *p;
     int status;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
     if (gethostname(hostname, sizeof(hostname)) != 0) {
-        perror("gethostname");
+        fprintf(stderr, "Error getting hostname: %s\n", strerror(errno));
         return;
     }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // Use AF_INET for IPv4, AF_INET6 for IPv6, AF_UNSPEC for both
+    hints.ai_socktype = SOCK_STREAM; // Can be set to 0 if socket type is irrelevant
 
     if ((status = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        fprintf(stderr, "Error getting address info: %s\n", gai_strerror(status));
         return;
     }
 
-    printf("IP Addresses:\n");
+    printf("IP Addresses for %s:\n", hostname);
     for (p = res; p != NULL; p = p->ai_next) {
         void *addr;
         char ipstr[INET6_ADDRSTRLEN];
@@ -86,43 +88,58 @@ static void printIPAddresses(void) {
         if (p->ai_family == AF_INET) {
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
             addr = &(ipv4->sin_addr);
-        } else {
+            inet_ntop(AF_INET, addr, ipstr, sizeof(ipstr));
+            printf("  IPv4: %s\n", ipstr);
+        } else if (p->ai_family == AF_INET6) {
             struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
             addr = &(ipv6->sin6_addr);
+            inet_ntop(AF_INET6, addr, ipstr, sizeof(ipstr));
+            printf("  IPv6: %s\n", ipstr);
         }
-
-        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-        printf("  %s\n", ipstr);
     }
 
     freeaddrinfo(res);
 }
 
 int main(int argc, char **argv) {
+    int opt;
+    int option_index = 0;
+
+    static struct option long_options[] = {
+        {"help", no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'v'},
+        {"ip-address", no_argument, 0, 'i'},
+        {"dnsdomainname", no_argument, 0, 'd'},
+        {0, 0, 0, 0}
+    };
+
+    // If no arguments are given, display the hostname
     if (argc == 1) {
         printShortHostname();
         return EXIT_SUCCESS;
-    } else if (argc == 2) {
-        if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) {
-            printVersion();
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-            printHelp();
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[1], "--dnsdomainname") == 0 || strcmp(argv[1], "-d") == 0) {
-            printDNSDomainName();
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[1], "--ip-address") == 0 || strcmp(argv[1], "-i") == 0) {
-            printIPAddresses();
-            return EXIT_SUCCESS;
-        } else {
-            fprintf(stderr, "Error: Unrecognized option '%s'\n", argv[1]);
-            fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-            return EXIT_FAILURE;
-        }
-    } else {
-        fprintf(stderr, "Error: Too many arguments\n");
-        fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-        return EXIT_FAILURE;
     }
+
+    // Parse command-line options
+    while ((opt = getopt_long(argc, argv, "hvid", long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 'h':
+                printHelp();
+                return EXIT_SUCCESS;
+            case 'v':
+                printVersion();
+                return EXIT_SUCCESS;
+            case 'i':
+                printIPAddresses();
+                return EXIT_SUCCESS;
+            case 'd':
+                printDNSDomainName();
+                return EXIT_SUCCESS;
+            default:
+                fprintf(stderr, "Unrecognized option\n");
+                printHelp();
+                return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_FAILURE;
 }
